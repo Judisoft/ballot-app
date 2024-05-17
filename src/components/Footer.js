1d08c45f5f87fbec63b75f2984ea6387c26ad825
Judisoft <kumjude09@gmail.com>
2024-05-17 15:40:44 +0100

    modified Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -21,7 +21,7 @@ const Footer = () => {
       <footer className="fixed bottom-0 left-0 w-full p-1 z-20  bg-gray-50 border-gray-200 md:flex md:items-center md:justify-between md:p-4  ">
         <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
           <div className="hidden md:block lg:block">
-            <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500  sm:mt-0">
+            {/* <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500  sm:mt-0">
               <li>
                 <Link to="/about" className="hover:underline me-4 md:me-6">
                   About
@@ -48,16 +48,27 @@ const Footer = () => {
                   Contact
                 </Link>
               </li>
-            </ul>
+            </ul> */}
           </div>
           <div className="mt-8 md:mt-0">
             <span className="text-sm text-gray-500 sm:text-center ">
               © {new Date().getFullYear()}
               <Link to="/" className="hover:underline">
                 {" "}
-                NjangiBallot.com
+                BallotApp
               </Link>
-              . All Rights Reserved.
+              <Link
+                to="#Licensing"
+                onClick={toggleLicenceModal}
+                className="hover:underline me-4 md:me-6">
+                Licensing
+              </Link>
+              <Link
+                to="#PrivacyPolicy"
+                onClick={togglePrivacyPolicyModal}
+                className="hover:underline me-4 md:me-6">
+                Privacy
+              </Link>
             </span>
           </div>
         </div>
