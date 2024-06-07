ccfef6db2e0f96ad395dbe89defb2cc6cc310852
Judisoft <kumjude09@gmail.com>
2024-06-07 18:02:14 +0100

    modify password placeholder for RegisterCard

--- src/components/RegisterCard.js
+++ src/components/RegisterCard.js
@@ -247,7 +247,7 @@ const RegisterCard = () => {
                     type="password"
                     name="password"
                     id="password"
-                    placeholder="••••••••"
+                    placeholder="password"
                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5    "
                     required="required"
                     value={password}
